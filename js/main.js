dc652938f5f32650b8d5ee73f15a90ce300d5910
Bogdan Kalinowski <bodok2@gmail.com>
2018-10-07 16:39:41 +0200

    warsztat2

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//mój sosób
+
+/*let par1 = document.getElementById("p1");
+	
+let par2 = 	document.getElementById("p2");
+	
+	
+function ustawTlo() {
+	
+	par1.style.backgroundColor="green";
+	
+	par2.style.backgroundColor="red";
+	
+}
+
+document.getElementsByTagName("button")[0].onclick = ustawTlo;
+
+//document.querySelector('button').onclick = ustawTlo;*/
+
+//sposób Uli
+
+
